26b1478534b77df2755f81e9bade10983cc4b9a5
Ilya Tikhobaev <dontixabon@gmail.com>
2022-11-21 13:20:16 +0300

    Added final report

--- BnB/main.py
+++ BnB/main.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
         "brock200_2.clq", "brock200_3.clq", "brock200_4.clq", "brock200_1.clq",
         "sanr200_0.7.clq"
     ]
-    filenames = ["C125.9.clq", "johnson16-2-4.clq"]
+    # filenames = ["C125.9.clq", "johnson16-2-4.clq"]
     heuristic_times, bnb_times, clique_sizes, cliques = [], [], [], []
     time_limit = 7000
     abs_tol = 1e-4
